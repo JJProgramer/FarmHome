4c5e195f65fae5a8c10ae68007eb66fdc4cac0cf
JJProgramer <osejnauj1@gmail.com>
2023-09-07 11:28:49 -0500

    bd

--- dist/model/conexion.php
+++ dist/model/conexion.php
@@ -1,10 +1,10 @@
 <?php 
-$contrasena = "";
-$usuario    = "root";
-$nombre_bd  = "farmhomedb";
+$contrasena = "242004Santi";
+$usuario    = "u985211323_Admin";
+$nombre_bd  = "u985211323_farmhomedb";
 
 try {
-	$conn = mysqli_connect('localhost','root','','farmhomedb');} catch (Exception $e) {
+	$conn = mysqli_connect('localhost','u985211323_Admin','242004Santi','u985211323_farmhomedb');} catch (Exception $e) {
 	echo "Hay algún problema con la conexión: ".$e->getMessage();
 }
 ?>
