87d58f51d2a0eecd67c137efae23bf010d9ad39e
JJProgramer <osejnauj1@gmail.com>
2023-09-19 00:52:18 -0500

    Se modifico la conexion

--- dist/model/conexion.php
+++ dist/model/conexion.php
@@ -1,10 +1,10 @@
 <?php
-$server ="localhost";
+$server =" 127.0.0.1";
 $contrasena = "Santiago2004";
 $usuario    = "u985211323_admin";
 $nombre_bd  = "u985211323_farmhomedb";
 
-$tabla_db1 = "registrouser";
+
 try {
 	$conn = mysqli_connect($server, $usuario, $contrasena, $nombre_bd);
 } catch (Exception $e) {
