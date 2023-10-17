3dd6f47e7c53264ce30be7346f417e5d7ea469a6
JJProgramer <osejnauj1@gmail.com>
2023-10-17 11:38:59 -0500

    la conexion ta buena kkakakaa

--- dist/model/conexion.php
+++ dist/model/conexion.php
@@ -1,5 +1,5 @@
-
-<!--$server ="localhost";
+<?php
+$server ="localhost";
 $contrasena = "Juanes2004";
 $usuario    = "u985211323_admin";
 $nombre_bd  = "u985211323_farmhomedb";
@@ -10,20 +10,17 @@ try {
 	$conn = mysqli_connect($server, $usuario, $contrasena, $nombre_bd);
 } catch (Exception $e) {
 	echo "Hay algún problema con la conexión: " . $e->getMessage();
-} -->
-<?php
+} ?>
 
 
-
-
+<!--
 $contrasena = "";
 $usuario    = "root";
 $nombre_bd  = "farmhomedb";
 
 $tabla_db1 = "registrouser"; 
 try {
-	$conn = new mysqli('localhost','root','','farmhomedb');} catch (Exception $e) {
+	$conn = mysqli_connect('localhost','root','','farmhomedb');} catch (Exception $e) {
 	echo "Hay algún problema con la conexión: ".$e->getMessage();
 }
-
-?>
+-->
